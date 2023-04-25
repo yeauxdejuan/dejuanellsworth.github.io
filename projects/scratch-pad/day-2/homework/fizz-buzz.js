c479962746327860f7b7bcc2ea9a45eb9bfb38fc
Dejuan  Ellsworth <dejuan.ells@gmail.com>
2023-04-25 13:33:48 +0000

    Day 2

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -10,11 +10,29 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
-function fizzBuzz() {
+/*
+    I: function takes in a number between 1-100
+    O: function returns 'fizz', 'buzz', and 'fizzbuzz' based on conditions met
+    C:
+    E: negative numbers and numbers 100+
+
+*/
+
+function fizzBuzz(num) {
     // YOUR CODE GOES BELOW HERE //
 
-
-
+    // after the for loop, the conditins should start with 15 so that 3/5 doesnt intercept the call before it reaches 15
+    for( let num = 1; num <= 100; num++ ){
+        if ( num % 15 === 0){ // num of 3 and 5  % = 0 is replaced by 15 for fizzbuzz and prioritzed to so that the call
+            console.log('FizzBuzz');
+        } else if (num % 5 === 0){
+            console.log('Buzz');
+        } else if (num % 3 === 0 ){
+            console.log('Fizz')
+        } else {
+            console.log(num)
+        }
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
