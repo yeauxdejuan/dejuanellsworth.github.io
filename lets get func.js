14a445ec25ab597f0fb9fdcb942820251ac8798d
yeauxdejuan <dejuan.ells@gmail.com>
2023-05-11 21:08:32 +0000

    lgf

---
+++ lets get func.js
@@ -0,0 +1,84 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+var customers = require('./data/customers.json');
+var _ = require('underbar');
+
+/**
+ * 1. Import your lodown module using the require() method,
+ *    using the string , or whatever
+ *    name with which you published your npm lodown project.
+ *
+ * 2. Solve all problems as outlined in the README.
+ *
+ * 3. We started the first one for you as an example! Make the rest in that style.
+ *
+ * 4. To test your work, run the following command in your terminal:
+ *
+ *    npm start --prefix ./<YOUR_GITHUB_FOLDER>
+ *
+ *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
+ */
+
+var maleCount = (array) => {
+    let arr = []
+    _.filter(array, (customers) => {
+        if(customers.gender === 'male' ){
+            arr.push(customers)
+        }
+    })
+
+    return arr.length
+}
+
+var femaleCount = (array) => {
+
+    const females = array.reduce(
+        (accum, curr) => accum.concat(curr), [])
+        .filter(item => item.gender === 'female').length
+
+   return females
+
+};
+
+var oldestCustomer = (array) => {
+
+// _.filter(array, (customer) => {
+//     let arr = []
+//     if(Math.min(customer.age))
+// })
+let i = array.indexOf(Math.max(...array.age));
+    
+return i
+};
+
+var youngestCustomer;
+
+var averageBalance;
+
+var firstLetterCount;
+
+var friendFirstLetterCount;
+
+var friendsCount;
+
+var topThreeTags;
+
+var genderCount;
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+// here, export any references you need for tests //
+module.exports.maleCount = maleCount;
+module.exports.femaleCount = femaleCount;
+module.exports.oldestCustomer = oldestCustomer;
+module.exports.youngestCustomer = youngestCustomer;
+module.exports.averageBalance = averageBalance;
+module.exports.firstLetterCount = firstLetterCount;
+module.exports.friendFirstLetterCount = friendFirstLetterCount;
+module.exports.friendsCount = friendsCount;
+module.exports.topThreeTags = topThreeTags;
+module.exports.genderCount = genderCount;
