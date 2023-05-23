1cf5e1d8d92431402b344025ded1816b41d598b7
yeauxdejuan <dejuan.ells@gmail.com>
2023-05-23 14:45:41 +0000

    studies

---
+++ studies/datatypes.js
@@ -0,0 +1,35 @@
+/*
+
+
+//                                                           J A V A S C R I P T    D A T A   T Y P E S
+
+
+//                                                          Type	    typeof value	    Object wrapper
+//                                                           Null	    "object"	        N/A
+//                                                           Undefined	"undefined" 	    N/A
+//                                                           Boolean	"boolean"   	    Boolean
+//                                                           Number	    "number"	        Number
+//                                                           BigInt 	"bigint"	        BigInt
+//                                                           String 	"string"	        String
+//                                                           Symbol 	"symbol"	        Symbol
+
+
+
+SIMPLE/ PRIMITIVE DATA TYPES 
+
+'HELLO WORLD' <= STRING     The String type represents textual data. Each element in the string occupies a position in the string. The first element is at index 0, the next at index 1, and so on. 
+TRUE-FALSE    <= BOOLEAN    Represents a logical entity and is inhabited by two values: true and false.
+0123456789    <= NUMBER     Capable of storing positive floating-point numbers
+UNDEFINED    <= UNDEFINED   Undefined indicates the absence of a value,
+NULL          <= NULL       The Null type is inhabited by exactly one value: null
+SYMBOL('VAL') <= SYMBOL     Represents a unique and immutable primitive value and may be used as the key of an Object
+123456...890n <= BIGINT     Represents numeric values that can represent integers with arbitrary magnitude. 
+
+
+
+COMPLEX DATA TYPES
+{}      <= OBJECT           Objects are used to store key-value paired data
+[]      <= ARRAY            Arrays are used to store a collection of any data type
+*/
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
