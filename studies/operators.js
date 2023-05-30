29cc799c21c5fbfe07e3419abc6d611cdcda6643
yeauxdejuan <dejuan.ells@gmail.com>
2023-05-30 04:26:54 +0000

    studies

---
+++ studies/operators.js
@@ -0,0 +1,57 @@
+/*
+
+//                  A R I T H M E T I C    O P E R A T O R S
+
+Arithmetc operators are used to complete mathematical operations between numberic values.
+
+    add operator:  + adds two numbers values 
+    minus operator: - subtracts right value from the left 
+    multiplies operator: * multiplies to values 
+    divide operator: / divides left value from right value
+    Modulus operator: % returns only the remainder after dividing two values 
+    increment: ++ increase value by one
+    decrement: -- decreases value by one 
+
+//                  C O M P A R I S O N   O P E R A T O R S 
+
+Comparison operators returns a boolean value after comparing two values 
+
+loosely equals: == Compares the equality of two operands without considering type.
+strickly equals: === Compares equality of two operands with type.
+Doesnt equal: != Compares inequality of two operands.
+Greater than: >	Returns a boolean value true if the left-side value is greater than the right-side value; otherwise, returns false.
+Less than : <	Returns a boolean value true if the left-side value is less than the right-side value; otherwise, returns false.
+Grater than or equl to: >=	Returns a boolean value true if the left-side value is greater than or equal to the right-side value; otherwise, returns false.
+Less than or equal to: <=	Returns a boolean value true if the left-side value is less than or equal to the right-side value; otherwise, returns false.
+
+
+
+//                  L O G I C A L   O P E R A T O R S
+
+Logical operators combine two or more conditions 
+
+AND operator: && checks whether two operands are non-zero or not. It returns 1 if they are non-zero; otherwise, returns 0.
+OR opertor:|| checks whether any one of the two operands is non-zero or not. It returns 1 if any one of of them is non-zero; otherwise, returns 0.
+Bang operator:!  reverses the boolean result of the operand 
+
+
+
+//                        A S S I G N M E N T  O P E R A T O R S
+
+equal assign: =	Assigns right operand value to the left operand.
+add asign: +=	Sums up left and right operand values and assigns the result to the left operand.
+minues assign: -=	Subtract right operand value from the left operand value and assigns the result to the left operand.
+multiple assign: *=	Multiply left and right operand values and assigns the result to the left operand.
+divide assign: /=	Divide left operand value by right operand value and assign the result to the left operand.
+modulo assign: %=	Get the modulus of left operand divide by right operand and assign resulted modulus to the left operand.
+
+
+//                           T E R N A R Y  O P E R A T O R
+
+
+The ternary op is the same as a if:else if: else 
+
+<condition> ? <return val> : <else condition return val>
+
+
+*/
