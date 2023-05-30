30f2798f654890460b6c8e688b4e4082d460f0d0
yeauxdejuan <dejuan.ells@gmail.com>
2023-05-30 04:57:39 +0000

    studies

---
+++ studies/string-manipulation.js
@@ -0,0 +1,29 @@
+/*
+
+
+//          J A V A S C R I P T  S T R I N G   M A N I P U L A T I O N
+
+string.length => returns the length of a srting based on its character count
+
+string.slice(start, stop) => extracts a part of string and returns the extracted value
+
+string.substring() => extracts part of a string, as slice does. second param is length 
+
+string.replace() => replaces a specific value from a string with another value (only first occurance)
+
+string.replaceAll() =>  replaces every occurance of a specific value in a string
+
+string.toLowerCase => returns string value to lower case
+
+string.toUpperCase() => returns string value to upper case
+
+string.concat() => joins two or more strings together
+
+string.charAt() => returns a character at a specified index
+
+string.split() => returns a new array with specified divider 
+ 
+
+
+
+*/
